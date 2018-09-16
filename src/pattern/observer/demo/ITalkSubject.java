7a8c70f20999c49fffad729942a7d5d3ec64c285
liweihai <liweihai12345@sina.com>
2018-09-17 07:49:21 +0800

    添加注释

--- src/pattern/observer/demo/ITalkSubject.java
+++ src/pattern/observer/demo/ITalkSubject.java
@@ -5,8 +5,11 @@ package pattern.observer.demo;
  *
  */
 public interface ITalkSubject {
-
+	
+	// 注册观察者
 	public void registerObserver(ITalkObserver observer);
+	//删除观察者
 	public void deleteObserver(ITalkObserver observer);
+	//修改观察者
 	public void notifyObserver();
 }
